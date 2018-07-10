d43d2b3b2b614b99147093fce701053194b498f9
Shital <s28tiwari@gmail.com>
2018-07-11 00:26:15 +0530

    Pod Setup and Pod Installation
    Initiated pod setup to generate pod file. This pod file contains framework specifications which will be used in project
    In this project pod file contains frameworks to check network availability using "ReachabilitySwift" and
    "SVProgressHUD" is added to show progress indicator to inform activity is in process.
    Bridging-Header file is added to the project so as to allow application to use Objective-C classes from "SVProgressHUD" framework in SWIFT classes.
    Bridging-Header file name is updated in project target settings against "Objective-C Bridging Header" so as application will know the file's location

---
+++ Wipro_Assignment/Wipro_Assignment-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Wipro_Assignment-Bridging-Header.h
+//  Wipro_Assignment
+//
+//  Created by SierraVista Technologies Pvt Ltd on 11/07/18.
+//  Copyright © 2018 Shital. All rights reserved.
+//
+
+#ifndef Wipro_Assignment_Bridging_Header_h
+#define Wipro_Assignment_Bridging_Header_h
+
+
+#endif /* Wipro_Assignment_Bridging_Header_h */
